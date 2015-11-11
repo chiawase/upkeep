42c0cbee5ae9be329089357ccf4c8a43cd27a5d2
chiawase <chi.senires@gmail.com>
2015-11-11 17:57:22 +0800

    Set up About Company andContact Us pages

--- main.js
+++ main.js
@@ -14,4 +14,16 @@ $(document).ready( function() {
 			bottom: '-11.5%'
 		}, 600, 'swing');
 	});
+
+	var scroll = $(target).offset().bottom;
+	$('html, body').scroll(function (e) {
+		alert(scroll)
+		var st = $(this).scrollTop();
+		if (st > scroll) {
+			$('footer').animate({bottom: '-=10'}, 10);
+		} else {
+			$('footer').animate({bottom: '+=10'}, 10);
+		}
+		scroll = st;
+	});
 });
